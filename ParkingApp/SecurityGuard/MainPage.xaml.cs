e129a98f5d50e9f850833421dcc9c4c47db7b7ef
cgomez374 <44035221+cgomez374@users.noreply.github.com>
2020-02-03 10:25:00 -0800

    Add files via upload

---
+++ ParkingApp/SecurityGuard/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SpotLotSecurityGuard
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void OnButtonClickedParkingLot(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ParkingLots());
+        }
+
+        public async void OnButtonClickedCitations(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Citations());
+        }
+
+        public async void OnButtonClickedTowing(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Towing());
+        }
+
+        public async void OnButtonClickedAlerts(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecurityAlerts());
+        }
+    }
+}
---
+++ ParkingApp/SecurityGuard/Towing.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SpotLotSecurityGuard
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Towing : ContentPage
+    {
+        public Towing()
+        {
+            InitializeComponent();
+        }
+    }
+}
